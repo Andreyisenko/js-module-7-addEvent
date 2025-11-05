bab04b7ec5c42fd304cdf7d3b078ebd7c8854f66
Andrey Isenko <aisenko87@gmail.com>
2025-11-05 19:52:07 +0200

    add-task

--- module-7-2-event.js
+++ module-7-2-event.js
@@ -1,29 +1,31 @@
-const inpt = document.querySelector(".form");
-inpt.addEventListener("submit", changeInpit);
+// const inpt = document.querySelector(".form");
+// inpt.addEventListener("submit", changeInpit);
 
-function changeInpit(event) {
-  event.preventDefault();
-  const iNput = event.target;
-  console.log(iNput);
-  console.log(event.target.elements.login.value);
-  console.log(event.target.elements.password.value);
-  inpt.reset();
-}
-const loginInpt = document.querySelector(".formInput");
-console.log(loginInpt);
-const allInpt = document.querySelectorAll(".formInput");
-allInpt.forEach((elem) => elem.addEventListener("input", changePress));
+// function changeInpit(event) {
+//   event.preventDefault();
+//   const iNput = event.target;
+//   console.log(iNput);
+//   console.log(event.target.elements.login.value);
+//   console.log(event.target.elements.password.value);
+//   inpt.reset();
+// }
+// const loginInpt = document.querySelector(".formInput");
+// console.log(loginInpt);
+// const allInpt = document.querySelectorAll(".formInput");
+// allInpt.forEach((elem) => elem.addEventListener("input", changePress));
 
-// inpt.addEventListener("keypress", changePress);
-// inpt.addEventListener("keyup", changePress);
-// loginInpt.addEventListener("keydown", changePress);
+// // inpt.addEventListener("keypress", changePress);
+// // inpt.addEventListener("keyup", changePress);
+// // loginInpt.addEventListener("keydown", changePress);
 
-function changePress(event) {
-  console.log(event.target.value);
-}
+// function changePress(event) {
+//   console.log(event.target.value);
+// }
 
-loginInpt.addEventListener("change", foo);
+// loginInpt.addEventListener("change", foo);
 
-function foo(event) {
-  console.log("ok");
-}
+// function foo(event) {
+//   console.log(event.target.value);
+
+//   console.log("ok");
+// }
